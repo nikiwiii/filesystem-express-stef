20b0c91ab21f00b0dc048845e08202b5aeb85799
Tymoteusz Sowa <tsowa@sala514i.zsl>
2023-04-21 15:23:53 +0200

    add copies

--- server.js
+++ server.js
@@ -14,6 +14,7 @@ app.engine('hbs', hbs.engine({
 }));
 
 
+
 // fs.unlink(filepath,  (err) =>{
 //     if (err) throw err
 //     console.log("czas 1: " + new Date().getMilliseconds());
@@ -22,9 +23,25 @@ app.engine('hbs', hbs.engine({
 
 app.get("/", (req, res) => {
     res.render('index.hbs', {
-        onmain: true
+        files: segregate()
     })
 })
+
+const segregate = () => {
+    var tab = fs.readdirSync('./upload/')
+    var folders = []
+    var files = []
+    tab.forEach(e => {
+        if (fs.lstatSync('./upload/'+e).isDirectory()) {
+            folders.push({name: e, type: true})
+        }
+        else {
+            files.push({name: e, type: false})
+        }
+    });
+    console.log(folders,files);
+    return [folders,files]
+}
 
 app.use(express.urlencoded({
     extended: true
@@ -38,14 +55,35 @@ app.post('/newfolder', (req, res) => {
             console.log("jest");
         })
     }
-
-    // cars.insert(data, (_, doc) => {
-    //     res.render('index.hbs', {
-    //         onadd: true,
-    //         id: doc._id
-    //     })
-    // })
+    else {
+        fs.mkdir("./upload/" + req.body.name + '_kopia_' + Date.now, (err) => {
+            if (err) throw err
+            console.log("jest");
+        })
+    }
+    res.render('index.hbs', {
+        files: segregate()
+    })
 })
+
+app.post('/newfile', (req, res) => {
+    if (!fs.existsSync("./upload/" + req.body.name)) {
+        fs.appendFile("./upload/" + req.body.name, '', (err) => {
+            if (err) throw err
+            console.log("jest");
+        })
+    }
+    else {
+        fs.appendFile("./upload/" + req.body.name + '_kopia_' + Date.now, '', (err) => {
+            if (err) throw err
+            console.log("jest");
+        })
+    }
+    res.render('index.hbs', {
+        files: segregate()
+    })
+})
+
 
 app.listen(port, () => {
     console.log(`Server listening on port: ${port}`)
