cfccac58dbc4cd13f700910af1d7aca5289524ba
nikiwiii <74990279+nikiwiii@users.noreply.github.com>
2023-05-22 00:17:38 +0200

    .

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const hbs = require('express-handlebars');
 const fs = require('fs');
 const path = require('path');
 const app = express();
+const multer = require('multer')
 const port = 4000;
 
 app.set('views', path.join(__dirname, 'views'));
@@ -15,6 +16,7 @@ app.engine(
   })
 );
 let currentPath = './upload/';
+let currentFile = ''
 
 const segregate = () => {
   let folders = [];
@@ -25,20 +27,18 @@ const segregate = () => {
       folders.push({
         name: e,
         type: true,
-        path:
-          currentPath.substr(9, currentPath.length).replaceAll('/', '~') + e, //query zachowa path z '~' zamiast '/'
+        path: currentPath.substr(9, currentPath.length).replaceAll('/', '~') + e, //query zachowa path z '~' zamiast '/'
       });
     } else {
       files.push({
-        name: e.substr(0, e.indexOf('.')),
-        format: e.substr(e.indexOf('.'), e.length),
+        name: e.substr(0, e.lastIndexOf('.')),
+        format: e.substr(e.lastIndexOf('.'), e.length),
         type: false,
-        html: e.substr(e.indexOf('.') + 1, e.length) === 'html' ? true : false,
-        css: e.substr(e.indexOf('.') + 1, e.length) === 'css' ? true : false,
-        js: e.substr(e.indexOf('.') + 1, e.length) === 'js' ? true : false,
+        html: e.substr(e.lastIndexOf('.') + 1, e.length) === 'html' ? true : false,
+        css: e.substr(e.lastIndexOf('.') + 1, e.length) === 'css' ? true : false,
+        js: e.substr(e.lastIndexOf('.') + 1, e.length) === 'js' ? true : false,
         fullname: e,
-        path:
-          currentPath.substr(9, currentPath.length).replaceAll('/', '~') + e,
+        path: currentPath.substr(9, currentPath.length).replaceAll('/', '~') + e,
       });
     }
   });
@@ -127,10 +127,10 @@ app.post('/newfile', (req, res) => {
   } else {
     fs.appendFile(
       currentPath +
-        name.substr(0, name.indexOf('.')) +
-        '_kopia_' +
-        new Date().valueOf() +
-        name.substr(name.indexOf('.'), name.length),
+      name.substr(0, name.lastIndexOf('.')) +
+      '_kopia_' +
+      new Date().valueOf() +
+      name.substr(name.lastIndexOf('.'), name.length),
       '',
       (err) => {
         if (err) throw err;
@@ -148,7 +148,10 @@ app.post('/newfile', (req, res) => {
 app.get('/folder&name=:name', (req, res) => {
   const name = req.params.name;
   if (fs.existsSync(currentPath + name)) {
-    fs.rm(currentPath + name, { recursive: true, force: true }, (err) => {
+    fs.rm(currentPath + name, {
+      recursive: true,
+      force: true
+    }, (err) => {
       if (err) throw err;
       console.log('usunieto ' + name);
       res.render('index.hbs', {
@@ -156,6 +159,12 @@ app.get('/folder&name=:name', (req, res) => {
         pathArr: getPathArr(),
         nonuploadfolder: currentPath.length !== 9 ? true : false,
       });
+    });
+  } else {
+    res.render('index.hbs', {
+      files: segregate(),
+      pathArr: getPathArr(),
+      nonuploadfolder: currentPath.length !== 9 ? true : false,
     });
   }
 });
@@ -171,60 +180,46 @@ app.get('/file&name=:name', (req, res) => {
         nonuploadfolder: currentPath.length !== 9 ? true : false,
       });
     });
+  } else {
+    res.render('index.hbs', {
+      files: segregate(),
+      pathArr: getPathArr(),
+      nonuploadfolder: currentPath.length !== 9 ? true : false,
+    });
   }
 });
 
-app.post('/uploadf', (req, res) => {
-  if (!fs.existsSync(currentPath + req.body.filefold)) {
-    fs.appendFile(currentPath + req.body.filefold, '', (err) => {
-      if (err) throw err;
-      console.log('wrzucono ' + req.body.filefold);
-      // let text = fs.readFileSync(req.body.filefold[0]).toString('utf8');
-      // fs.writeFile(currentPath + req.body.filefold, text, (err) => {
-      //   res.render('index.hbs', {
-      //     files: segregate(),
-      //     pathArr: getPathArr(),
-      //     nonuploadfolder: currentPath.length !== 9 ? true : false,
-      //   });
-      // });
 
-      res.render('index.hbs', {
-        files: segregate(),
-        pathArr: getPathArr(),
-        nonuploadfolder: currentPath.length !== 9 ? true : false,
-      });
-    });
-  } else {
-    fs.appendFile(
-      currentPath + req.body.filefold + '_kopia_' + new Date().valueOf(),
-      '',
-      (err) => {
-        if (err) throw err;
-        console.log('wrzucono kopie ' + req.body.filefold);
-        // let text = fs.readFileSync(req.body.filefold).toString('utf8');
-        // fs.writeFile(currentPath + req.body.filefold, text, (err) => {
-        //   res.render('index.hbs', {
-        //     files: segregate(),
-        //     pathArr: getPathArr(),
-        //     nonuploadfolder: currentPath.length !== 9 ? true : false,
-        //   });
-        // })
+let upload = multer({
+  dest: currentPath
+});
+let type = upload.single('filefold');
+
+
+app.post('/uploadf', type, function (req, res) {
+  console.log(req.file);
+  let temp_file = req.file.path;
+  let name = req.file.originalname
+  let target_file = currentPath + (name.includes('.') ? (name.substr(0, name.lastIndexOf('.')) + '_' + req.file.filename.substr(0, 4) + name.substr(name.lastIndexOf('.'), name.length)) : (name + '_' + req.file.filename.substr(0, 4) + '.txt'));
+  fs.readFile(temp_file, (err, data) => {
+    fs.unlink(temp_file, (err) => {
+      fs.appendFile(target_file, data, (err) => {
         res.render('index.hbs', {
           files: segregate(),
           pathArr: getPathArr(),
           nonuploadfolder: currentPath.length !== 9 ? true : false,
         });
-      }
-    );
-  }
-});
+      })
+    });
+  });
+})
 
 app.get('/name=:path', (req, res) => {
   currentPath =
     './upload' +
-    (req.params.path[0] === '~'
-      ? req.params.path.replaceAll('~', '/')
-      : '/' + req.params.path.replaceAll('~', '/'));
+    (req.params.path[0] === '~' ?
+      req.params.path.replaceAll('~', '/') :
+      '/' + req.params.path.replaceAll('~', '/'));
   currentPath[currentPath.length - 1] !== '/' ? (currentPath += '/') : null;
   console.log(currentPath);
   res.render('index.hbs', {
@@ -237,12 +232,9 @@ app.get('/name=:path', (req, res) => {
 app.post('/newfoldername', (req, res) => {
   fs.rename(
     currentPath,
-    currentPath.substr(
-      0,
-      currentPath.slice(0, currentPath.length - 1).lastIndexOf('/')
-    ) +
-      '/' +
-      req.body.name,
+    currentPath.substr(0, currentPath.slice(0, currentPath.length - 1).lastIndexOf('/')) +
+    '/' +
+    req.body.name,
     (err) => {
       if (err) throw err;
       currentPath =
@@ -263,17 +255,60 @@ app.post('/newfoldername', (req, res) => {
 });
 
 app.get('/edit=:path', (req, res) => {
-  console.log(currentPath + req.params.path);
-  console.log(fs.existsSync(currentPath + req.params.path));
+  // console.log(currentPath + req.params.path);
+  // console.log(fs.existsSync(currentPath + req.params.path));
+  currentFile = currentPath + req.params.path
   fs.readFile(currentPath + req.params.path, (err, data) => {
     if (err) throw err;
-    console.log(data.toString('utf8'));
     res.render('edytor.hbs', {
       path: currentPath + req.params.path,
-      contents: data,
+      contents: data.toString('utf8'),
+      currentFile: currentFile.substr(currentFile.lastIndexOf('/')+1, currentFile.length)
     });
   });
 });
+
+let editorFontSize = 14
+let editorColor = 2
+
+app.post('/sendSettings', (req,res) => {
+  //body sie nie przekazuje samo wiec robie takie cos przepraszam
+  let sentStuff = JSON.parse(req.headers.body)
+  editorColor = sentStuff.color
+  editorFontSize = sentStuff.size
+  res.send(JSON.stringify('zapisano'))
+})
+
+app.get('/getSettings', (req,res) => {
+  res.send(JSON.stringify({
+    size: editorFontSize,
+    color: editorColor
+  }, null, 5))
+})
+
+app.post('/sendChanged', (req,res) => {
+  const data = JSON.parse(req.headers.body).newText
+  fs.writeFile(currentFile, data, (err) => {
+    res.send(JSON.stringify('zapisano zmiany'))
+  })
+})
+
+app.post('/newfilename', (req,res) => {
+  console.log(currentFile.substr(currentFile.lastIndexOf('/')+1, currentFile.length));
+  console.log(req.body.name);
+  fs.rename(currentFile, currentFile.substr(0, currentFile.lastIndexOf('/')+1) +  req.body.name, (err) => {
+    if (err) throw err
+    currentFile = currentPath + req.body.name
+    fs.readFile(currentPath + req.body.name, (err, data) => {
+      if (err) throw err;
+      res.render('edytor.hbs', {
+        path: currentPath + req.body.name,
+        contents: data.toString('utf8'),
+        currentFile: currentFile.substr(currentFile.lastIndexOf('/')+1, currentFile.length)
+      });
+    });
+  }) 
+})
 
 app.listen(port, () => {
   console.log(`Server listening on port: ${port}`);
--- static/script.js
+++ static/script.js
@@ -3,7 +3,7 @@ const form = document.querySelector('#new');
 const btn1 = document.querySelector('#addfolder');
 const btn2 = document.querySelector('#addfile');
 const delfolders = document.querySelector('#folder');
-const delfiles = document.querySelector('#files');
+const delfiles = document.querySelector('#dfiles');
 const cancelBtn1 = document.querySelector('#cancel1');
 const cancelBtn2 = document.querySelector('#cancel2');
 const upload = document.querySelector('#upload');
@@ -12,37 +12,139 @@ const delfile = document.querySelector('#delfilebtn');
 const delfolder = document.querySelector('#delfolderbtn');
 const nameFormBtn = document.querySelector('#newnamebtn');
 const nameForm = document.querySelector('#new-element-name');
-const actualBtn = document.getElementById('file-upload');
+const actualForm = document.getElementById('file-upload');
 const fileChosen = document.getElementById('file-chosen');
+const textArea = document.getElementById('text-contents');
+const lineCounter = document.getElementById('line-counter');
+const changecolor = document.getElementById('changecolor');
+const styleSwitcher = document.getElementById('style-switcher');
+const fontAdd = document.getElementById('add')
+const fontLessen = document.getElementById('less')
+const saveSettings = document.getElementById('saveSettings')
+const saveChanges = document.getElementById('saveChanges')
+const fileNameDialog = document.getElementById('new-file-name')
+const renameFile = document.getElementById('renameFile')
+const cancelBtn3 = document.getElementById('cancel3')
 
-actualBtn.addEventListener('change', () => {
-  fileChosen.textContent = this.files[0].name;
-});
+if (fontLessen){
+  var fontSize = 14
+  var styleIte = 0
+  let lines = textArea.value.split("\n");
+  let val = 1
 
-upload.addEventListener('click', () => {
-  if (document.querySelector('#file-upload').value) {
-    upload.type = 'submit';
-    uploadform.submit();
+  updateFont = () => {
+    lineCounter.style.fontSize = String(fontSize) + 'px'
+    lineCounter.style.height = String(val * fontSize * 1.3) + 'px'
+    lineCounter.style.width = String(fontSize * 2) + 'px'
+    textArea.style.fontSize = String(fontSize) + 'px'
+    textArea.style.height = String(val * fontSize * 1.3) + 'px'
   }
-});
-cancelBtn1.addEventListener('click', () => {
-  dialog.close();
-});
-btn1.addEventListener('click', () => {
-  document.querySelector('#dialogtext').innerHTML = 'nazwa nowego folderu:';
-  dialog.showModal();
-});
-btn2.addEventListener('click', () => {
-  form.action = '/newfile';
-  document.querySelector('#dialogtext').innerHTML = 'nazwa nowego pliku:';
-  dialog.showModal();
-});
-function logdelete(event) {
-  confirm('Jesteś pewny, że chcesz to usunąć?') ? event.submit() : null;
+
+  lines.forEach(e => {
+    lineCounter.value += String(val) + '\n'
+    val++
+  });
+
+  changecolor.addEventListener('click', () => {
+    if(styleIte != 3) {
+      styleIte++
+      styleSwitcher.classList.remove(`style${styleIte-1}`)
+      styleSwitcher.classList.add(`style${styleIte}`)
+    }
+    else {
+      styleIte = 0
+      styleSwitcher.classList.remove(`style3`)
+      styleSwitcher.classList.add(`style${styleIte}`)
+    }
+    console.log(styleIte);
+  })
+
+  saveChanges.addEventListener('click', async() => {
+    const response = await fetch('http://192.168.10.112:4000/sendChanged', {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+        body: JSON.stringify({
+          newText: textArea.value
+        }),
+      },
+    });
+    const res = await response.json()
+    alert(res);
+  })
+
+  renameFile.addEventListener('click', () => {
+    fileNameDialog.showModal()
+  })
+
+  cancelBtn3.addEventListener('click', () => {
+    fileNameDialog.close()
+  })
+
+  saveSettings.addEventListener('click', async() => {
+    const response = await fetch('http://192.168.10.112:4000/sendSettings', {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+        body: JSON.stringify({
+          size: fontSize,
+          color: styleIte,
+        }),
+      },
+    });
+    const res = await response.json()
+    alert(res);
+  })
+  
+  refreshAll = async() => {
+    const response = await fetch('http://192.168.10.112:4000/getSettings');
+    const json = await response.json();
+    console.log(json);
+    styleIte = json.color
+    fontSize = json.size
+  
+    fontAdd.addEventListener('click', () => {fontSize++; updateFont()})
+    fontLessen.addEventListener('click', () => {fontSize--; updateFont()})
+    styleSwitcher.classList.add(`style${styleIte}`)
+    updateFont()
+  }
+  refreshAll()
 }
-nameFormBtn.addEventListener('click', () => {
-  nameForm.showModal();
-});
-cancelBtn2.addEventListener('click', () => {
-  nameForm.close();
-});
+else {
+  actualForm.addEventListener('change', () => {
+    fileChosen.textContent = actualForm.value;
+  });
+
+  upload.addEventListener('click', () => {
+    if (document.querySelector('#file-upload').value) {
+      upload.type = 'submit';
+      uploadform.submit();
+    }
+  });
+  cancelBtn1.addEventListener('click', () => {
+    dialog.close();
+  });
+  btn1.addEventListener('click', () => {
+    document.querySelector('#dialogtext').innerHTML = 'nazwa nowego folderu:';
+    dialog.showModal();
+  });
+  btn2.addEventListener('click', () => {
+    form.action = '/newfile';
+    document.querySelector('#dialogtext').innerHTML = 'nazwa nowego pliku:';
+    dialog.showModal();
+  });
+  function logdelete(event) {
+    confirm('Jesteś pewny, że chcesz to usunąć?') ? event.submit() : null;
+  }
+  if (nameFormBtn){
+    nameFormBtn.addEventListener('click', () => {
+      nameForm.showModal();
+    });
+  }
+
+  cancelBtn2.addEventListener('click', () => {
+    nameForm.close();
+  });
+}
